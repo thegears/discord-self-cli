4c5906255f6ea078d435c152c62ac557fe44f2ff
thegears <semh.1362@gmail.com>
2022-11-05 16:07:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,60 +2,58 @@ const inquirer = require("inquirer");
 const fetch = require("node-fetch");
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({
-	checkUpdate: false
+    checkUpdate: false
 });
 inquirer.registerPrompt('search-list', require('inquirer-search-list'));
 const token = "";
-
-let dmuser;
 
 client.login(token);
 
 (async () => {
 
-	let friendsReq = await fetch("https://discord.com/api/v9/users/@me/relationships", {
-		headers: {
-			"Authorization": token
-		}
-	});
+    let friendsReq = await fetch("https://discord.com/api/v9/users/@me/relationships", {
+        headers: {
+            "Authorization": token
+        }
+    });
 
-	friendsReq = await friendsReq.json();
+    friendsReq = await friendsReq.json();
 
-	let friends = friendsReq.map(f => ({
-		name: `${f.user.username}#${f.user.discriminator}`,
-		value: f.id
-	}));
+    let friends = friendsReq.map(f => ({
+        name: `${f.user.username}#${f.user.discriminator}`,
+        value: f.id
+    }));
 
-	client.on("ready", async () => {
-		await console.clear();
-		inquirer.prompt(
-			{
-				type: 'search-list',
-				message: "Select",
-				name: "user",
-				choices: friends
-			})
-			.then(async (answers) => {
-				console.clear();
+    client.on("ready", async () => {
+        await console.clear();
+        inquirer.prompt(
+            {
+                type: 'search-list',
+                message: "Select",
+                name: "user",
+                choices: friends
+            })
+            .then(async (answers) => {
+                console.clear();
 
-				var readline = require('readline');
+                var readline = require('readline');
   
-				var rl = readline.createInterface(
-					process.stdin, process.stdout);
+                var rl = readline.createInterface(
+                    process.stdin, process.stdout);
 
 
 
-				rl.on('line', (input) => {
-					if(input) client.users.cache.get(answers.user).send({
-						content: input
-					});
-				});
+                rl.on('line', (input) => {
+                    if(input) client.users.cache.get(answers.user).send({
+                        content: input
+                    });
+                });
 
-				client.on("messageCreate", (message) => {
-					if (message.channel.type == "DM" && message.author.id == answers.user) {
-						console.log(`${message.author.username}: ${message.content}`);
-					};
-				});
-			});
-	});
+                client.on("messageCreate", (message) => {
+                    if (message.channel.type == "DM" && message.author.id == answers.user) {
+                        console.log(`${message.author.username}: ${message.content} ${(message.attachments.map(r => r).length > 0) ? '(+Attachment)' : ''}`);
+                    };
+                });
+            });
+    });
 })();
